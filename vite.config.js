96ff1d7e0d55218a0e06b3011e2db503ec7e8312
Mehdi <mehdiboucherrourdev@gmail.com>
2025-10-12 16:40:07 +0200

    build: publish docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/' // important pour un User Site: mehdibhdev.github.io/
+  base: '/',                // site user = base à "/"
+  build: { outDir: 'docs' } // IMPORTANT: Pages lit /docs
 })
